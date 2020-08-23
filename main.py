972cb74181d1a59910cb329b7d652faa8ce20733
Janette Chairez <66131439+jmchairez@users.noreply.github.com>
2020-08-22 21:09:03 -0500

    First section complete total votes

--- main.py
+++ main.py
@@ -1,4 +1,26 @@
 import os
 import csv
 
-vote = 
+votes = os.path.join("..", "Resources", "election_data.csv")
+
+with open(votes) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    csv_header = next(csvfile)
+
+    num_rows = 0
+    for rows in csvreader:
+        num_rows += 1
+
+    unique_candidate = []
+
+    for row in csvreader:
+        if row[2] not in unique_can:
+            unique_candidate.append(row[2])
+
+def print_data(poll_data):
+    voter_id = int(poll_data[0])
+    country = str(poll_data[1])
+    candidate = str(poll_data[3])
+
+
+print(f"Total Votes: {num_rows}")  
