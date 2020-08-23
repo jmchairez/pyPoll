ac5caf930dc0883b20067a32b927decc2f8c03b5
Janette Chairez <66131439+jmchairez@users.noreply.github.com>
2020-08-22 23:18:03 -0500

    set my candidate total votes

--- main.py
+++ main.py
@@ -3,24 +3,30 @@ import csv
 
 votes = os.path.join("..", "Resources", "election_data.csv")
 
+
+
 with open(votes) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     csv_header = next(csvfile)
 
     num_rows = 0
+    khan = 0
+    correy = 0
+    li = 0
+    otooley = 0
+
     for rows in csvreader:
         num_rows += 1
 
-    unique_candidate = []
+        if (rows[2] == "Khan"):
+            khan += 1
+        elif (rows[2] == "Correy"):
+            correy += 1
+        elif (rows[2] == "Li"):
+            li += 1
+        else:
+            otooley += 1
 
-    for row in csvreader:
-        if row[2] not in unique_can:
-            unique_candidate.append(row[2])
+        
 
-def print_data(poll_data):
-    voter_id = int(poll_data[0])
-    country = str(poll_data[1])
-    candidate = str(poll_data[3])
-
-
-print(f"Total Votes: {num_rows}")  
+   
